d2b8369e84c9977fef4193e96fb96291b26df5d8
BRANDON PARKER <Brandon@dkparker.com>
2016-04-16 10:41:35 -0700

    updated package.json and added in router.js

---
+++ router.js
@@ -0,0 +1,45 @@
+const Router = module.exports = exports = function () {
+  this.routes = {
+    'GET': {},
+    'POST': {},
+    'PUT': {},
+    'PATCH': {},
+    'DELETE': {}
+  };
+};
+
+Router.prototype.get = function (routeName, cb) {
+  this.routes.GET[routename] = cb;
+  return this;
+};
+
+Router.prototype.post = function (routeName, cb) {
+  this.routes.POST[routename] = cb;
+  return this;
+};
+
+Router.prototype.put = function (routeName, cb) {
+  this.routes.PUT[routename] = cb;
+  return this;
+};
+
+Router.prototype.patch = function (routeName, cb) {
+  this.routes.PATCH[routename] = cb;
+  return this;
+};
+
+Router.prototype.delete = function (routeName, cb) {
+  this.routes.DELETE[routename] = cb;
+  return this;
+};
+
+Router.prototype.route = function () {
+  var routes = this.routes;
+  return function (req, res) {
+    if (typeof routes[req.method][req.url] === 'function') {
+      return routes [req.method][req.url](req, res);
+    res.writeHead(404);
+    res.write('Not found, yo');
+    res.end();
+  };
+};
